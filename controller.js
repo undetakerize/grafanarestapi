1e92cdf408b66f1a64fd25af0efa4faaabd51c7d
Adi <adi.hdns@gmail.com>
2021-08-04 13:56:14 +0700

    fix controller function res call lupa lol

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require("./res");
 var connection = require("./koneksi");
 
 exports.index = function (req, res) {
-  response.ok("rest api running");
+  response.ok("rest api running", res);
 };
