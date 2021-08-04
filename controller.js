94d10e73c210d8101cb12ad7931ffb36305a43ec
Adi <adi.hdns@gmail.com>
2021-08-04 14:08:33 +0700

    add tampilData

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require("./koneksi");
 exports.index = function (req, res) {
   response.ok("rest api running", res);
 };
+
+//get data
+exports.tampilData = function (req, res) {
+  connection.query("SELECT *FROM tb_siswa", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, response);
+    }
+  });
+};
